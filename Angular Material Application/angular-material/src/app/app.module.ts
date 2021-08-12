cf21a44073fd7f5fb5d72136c674124a4fbad6a8
rebeccatannous <rebeccca.tannous@lau.edu>
2021-08-12 07:28:29 +0300

    added reactive form

--- Angular Material Application/angular-material/src/app/app.module.ts
+++ Angular Material Application/angular-material/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar, MatSnackBarConfig} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormGroup,
+    FormControl,
+    Validators,
+    MatSnackBar,
+    MatSnackBarConfig
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular Material Application/angular-material/src/app/shared/crud-form.service.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarHorizontalPosition } from '@angular/material/snack-bar';
+
+export interface Student {
+  fullName: string;
+  dateOfBirth: string;
+  idCopy: File | null;
+  gpa: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudFormService {
+  message: string = 'Student Exists';
+  actionButtonLabel: string = 'Confirm';
+  action: boolean = true;
+  setAutoHide: boolean = true;
+  autoHide: number = 2000;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  index = -1;
+
+  constructor(private _snackBar: MatSnackBar) { }
+
+  ELEMENT_DATA: Student[] = [
+    {
+      fullName: 'John Smith',
+      dateOfBirth: '1/1/2001',
+      idCopy: null,
+      gpa: '4.0',
+    },
+    {
+      fullName: 'Jane Doe',
+      dateOfBirth: '2/2/2002',
+      idCopy: null,
+      gpa: '3.5',
+    },
+    {
+      fullName: 'Maria Stewart',
+      dateOfBirth: '3/3/2003',
+      idCopy: null,
+      gpa: '2.76',
+    },
+  ];
+
+  form: FormGroup = new FormGroup({
+    fullName: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(50),
+      Validators.pattern('[a-zA-Z0-9 ]+'),
+    ]),
+    dateOfBirth: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(2),
+      Validators.pattern('^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$. '),
+    ]),
+    idCopy: new FormControl(null, Validators.required),
+    gpa: new FormControl('', Validators.pattern('/^[0-4]\.\d\d$/')),
+  });
+
+  setIndex(i: any) {
+    this.index = i;
+  }
+
+  emptyForm() {
+    this.form.setValue({
+      student: new FormControl(''),
+      abbreviation: new FormControl(''),
+      dataFile: new FormControl(null),
+      direction: new FormControl(''),
+    });
+  }
+  open() {
+    let config = new MatSnackBarConfig();
+    config.horizontalPosition = this.horizontalPosition;
+    config.duration = this.setAutoHide ? this.autoHide : 0;
+    this._snackBar.open(this.message, this.action ? this.actionButtonLabel : undefined, config);
+  }
+
+  populate(student: Student) {
+    this.form.setValue(student);
+  }
+  isDuplicate(element: Student) {
+    for (let i = 0; i < this.ELEMENT_DATA.length; i++) {
+      if (
+        element.fullName.toLowerCase() ==
+     this.ELEMENT_DATA[i].fullName.toLowerCase()
+      )
+        return true;
+    }
+    return false;
+  }
+  update(student: Student) {
+    if (!this.isDuplicate(student)) {
+      this.ELEMENT_DATA[this.index] = student;
+    } else {
+      this.open();
+      console.log('exits');
+    }
+  }
+}
